175800b12c97ede573433fc0a4f51f4a713305ec
Austin Cummings <aac729@live.com>
2020-08-13 23:20:08 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,14 @@
 class Cat
   # code goes here
+
+  attr_accessor :mood
+  attr_reader :name, :owner 
+
+    def initialize(name, owner)
+      @name = name
+      @mood = "nervous"
+    end
+    def self.all 
+      self
+    end 
 end
